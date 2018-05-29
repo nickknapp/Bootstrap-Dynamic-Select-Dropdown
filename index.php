a32027e3b64b4e74fa85eee3b625d9c31231a0e3
NICK KNAPP <nickknapp@users.noreply.github.com>
2018-05-29 10:38:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+<script src="dynamic-select-dropdown.js"></script>
+<!-- Include the above in your HEAD tag -->
 
 <div class="row">
   <div class="col-md-6 mb-3">
